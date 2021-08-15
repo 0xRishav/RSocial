1bed519ee2616ecd8d84f2048f02bb99023fe6cd
Rishav Bharti <rshvbharti@gmail.com>
2021-08-15 23:30:12 +0530

    exported components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,8 @@ export { default as Signin } from "./Signin";
 export { default as Signup } from "./Signup";
 export { default as UploadPhoto } from "./UploadPhoto";
 export { default as Feed } from "./Feed";
+export { default as PostPage } from "./PostPage";
+export { default as Search } from "./Search";
+export { default as CreatePost } from "./CreatePost";
+export { default as Notifications } from "./Notifications";
+export { default as Profile } from "./Profile";
