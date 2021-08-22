7897640ba3bbd56cb692cbb2dbf65c96bec9a815
Rishav Bharti <rshvbharti@gmail.com>
2021-08-23 05:01:14 +0530

    refactor: refactored feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,200 +1,41 @@
-import { BottomButtons, Button, Loader, Logo, Post } from "../components";
+import { BottomButtons, Button, Logo, Navbar, Post } from "../components";
 import { NavDiv } from "./Landing";
-import { BiPaperPlane } from "react-icons/bi";
-import Likes from "../components/Likes";
+
 import Story from "../features/story/Story";
-import { fetchAllStories } from "../features/story/StorySlice";
 import { fetchAllPosts } from "../features/post/PostSlice";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import useWindowDimensions from "../custom-hooks/useWindowDimensions";
+import { loaderOptions } from "../utils/utils";
+import Loader from "react-loader";
 
 const Feed = () => {
+  const { width } = useWindowDimensions();
   const dispatch = useDispatch();
-  const { feed } = useSelector((state) => state.post);
-  useEffect(() => {
-    dispatch(fetchAllStories());
-  }, []);
+  const { feed, loading } = useSelector((state) => state.post);
+  console.log(feed);
   useEffect(() => {
     dispatch(fetchAllPosts());
   }, []);
-  FaakePostData.map((post) => console.log("mapped"));
   return (
     <div style={{ width: "100%" }}>
-      <NavDiv>
-        <Logo />
-        {/* <BiPaperPlane size={25} /> */}
-      </NavDiv>
-      {/* <Story /> */}
+      {<Loader loaded={!loading} options={loaderOptions} />}
+      {width > 725 ? (
+        <Navbar />
+      ) : (
+        <NavDiv>
+          <Logo />
+        </NavDiv>
+      )}
+
+      <Story />
       {feed.map((post, index) => (
-        <Post key={index} {...post} />
+        <Post key={index} {...post} user={post.user} onHomePage />
       ))}
       <div style={{ marginBottom: "5rem" }} />
-      <BottomButtons />
+      {width < 725 && <BottomButtons />}
     </div>
   );
 };
-
-export const FaakePostData = [
-  {
-    name: "Rishav Bharti",
-    username: "rishavbharti1",
-    location: "Gwalior",
-    caption:
-      "Endless Journey, A journey which is never be end. The shot taken in Samsung M31s",
-    photo:
-      "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    profilePic:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    likes: 22,
-    comments: [
-      {
-        name: "Shubham Devrani",
-        profile:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        comment: "Perfect alignment",
-        time: "2 hours ago",
-      },
-      {
-        name: "Shubham Devrani",
-        profile:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        comment: "Perfect alignment",
-        time: "2 hours ago",
-      },
-      {
-        name: "Shubham Devrani",
-        profile:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        comment: "Perfect alignment",
-        time: "2 hours ago",
-      },
-      {
-        name: "Shubham Devrani",
-        profile:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        comment: "Perfect alignment",
-        time: "2 hours ago",
-      },
-      {
-        name: "Shubham Devrani",
-        profile:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        comment: "Perfect alignment",
-        time: "2 hours ago",
-      },
-      {
-        name: "Shubham Devrani",
-        profile:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-        comment: "Perfect alignment",
-        time: "2 hours ago",
-      },
-    ],
-  },
-  {
-    name: "Rishav Bharti",
-    username: "rishavbharti1",
-    location: "Gwalior",
-    caption:
-      "Endless Journey, A journey which is never be end. The shot taken in Samsung M31s",
-    photo:
-      "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    profilePic:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    likes: 22,
-    comments: [
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-    ],
-  },
-  {
-    name: "Rishav Bharti",
-    username: "rishavbharti1",
-    location: "Gwalior",
-    caption:
-      "Endless Journey, A journey which is never be end. The shot taken in Samsung M31s",
-    photo:
-      "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    profilePic:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    likes: 22,
-    comments: [
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-    ],
-  },
-  {
-    name: "Rishav Bharti",
-    username: "rishavbharti1",
-    location: "Gwalior",
-    caption:
-      "Endless Journey, A journey which is never be end. The shot taken in Samsung M31s",
-    photo:
-      "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    profilePic:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    likes: 22,
-    comments: [
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-    ],
-  },
-  {
-    name: "Rishav Bharti",
-    username: "rishavbharti1",
-    location: "Gwalior",
-    caption:
-      "Endless Journey, A journey which is never be end. The shot taken in Samsung M31s",
-    photo:
-      "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    profilePic:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    likes: 22,
-    comments: [
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-    ],
-  },
-  {
-    name: "Rishav Bharti",
-    username: "rishavbharti1",
-    location: "Gwalior",
-    caption:
-      "Endless Journey, A journey which is never be end. The shot taken in Samsung M31s",
-    photo:
-      "https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    profilePic:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
-    likes: 22,
-    comments: [
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-      "You are good bro",
-    ],
-  },
-];
 
 export default Feed;
